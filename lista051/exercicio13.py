19358d8deb9a46bb07579cfa3e42d845bdf55044
Vinícius Xavier <vinix1508@gmail.com>
2023-08-22 09:52:51 -0300

    ver.2.1.

--- lista051/exercicio13.py
+++ lista051/exercicio13.py
@@ -2,15 +2,13 @@
 13) Desenvolver um programa que imprima a tabuada de 3 a 6.
 """
 
+tabu = 3
 contador = 1
 contador2 = 1
 
-print("Tábuada de 3:")
 while contador <= 10:
-    print(f"3 * {contador} = {3 * contador}")
+    print(f"{tabu} * {contador} = {tabu * contador}")
     contador = contador + 1
-
-print("\nTábuada de 6:")
-while contador2 <= 10:
-    print(f"6 * {contador2} = {6 * contador2}")
-    contador2 = contador2 + 1
+    if contador == 11 and tabu < 6:
+        contador = 1
+        tabu = tabu + 1
